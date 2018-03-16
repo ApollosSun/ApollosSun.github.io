c19688d49aa61a2c09f1f5e844893ad9f7a200ca
ApollosSun <lookmindfully@gmail.com>
2018-03-16 15:36:05 +0200

    2.0

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,18 @@
-function displayResult()
-      {
-        var length = document.getElementById('length').value;
-        var width = document.getElementById('width').value;
-        var height = document.getElementById('height').value;
-        var result = length * width * height;
-        document.getElementById('result').innerHTML = result;
-      }
+//if ( confirm("Would you like to give us your name?") ) {
+//    var name = prompt("What's your name?");
+//    alert("Hi, " + name);
+//} else {
+//    alert("Sorry, you can not get an access");
+//}
+
+function buttonClick (form) {
+    //document.getElementById("field").innerHTML = "Your Google request is https://www.google.ie/search?q=" + form.request.value;
+    var link = "https://www.google.ie/search?q=" + form.request.value;
+    window.location = link;
+}
+
+function Scroll (id) {
+	var OurOffset = 0;
+	$('html, body').animate({scrollTop: $(id).offset().top - OurOffset}, 1000);
+	return false;
+}
