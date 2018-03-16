656e2cf50ab72be49dbad4c4a3d486439db85795
ApollosSun <lookmindfully@gmail.com>
2018-03-16 15:38:09 +0200

    2.1

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	$("#push").click(function(){
+		$.get('words.txt', function(data) {
+        	var lines = data.split('\n');
+        	var index = Math.floor(Math.random()*lines.length);
+        	var word = lines[index];
+        	$("#word").text(word);
+        	$.get('translate.txt', function(data) {
+        		var lines = data.split('\n');
+                        if (lines[index]) {
+                                var word = lines[index];
+                        } else {
+                                var word = "";
+                        }
+        		$("#translation").text(word);
+    		});
+    	});
+
+	});
+
+	$.get('words.txt', function(data) {
+        	var lines = data.split('\n');
+        	var index = Math.floor(Math.random()*lines.length);
+        	var word = lines[index];
+        	$("#word").text(word);
+        	$.get('translate.txt', function(data) {
+        		var lines = data.split('\n');
+        		var word = lines[index];
+        		$("#translation").text(word);
+    		});
+    	});
+ });
